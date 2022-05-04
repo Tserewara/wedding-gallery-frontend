ead0a566a0072f1bc033ced2ef73db2e50d02774
Tserewara <alvaro.alf2@gmail.com>
2022-05-04 20:49:28 -0300

    feat: add logout button

--- src/presentation/components/gallery-header/gallery-header.vue
+++ src/presentation/components/gallery-header/gallery-header.vue
@@ -1,12 +1,27 @@
 <template>
   <header class="headerWrap">
     <h1>Wedding Gallery</h1>
+    <span v-if="isLoggedIn" @click="handleClick">logout</span>
   </header>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "GalleryHeader",
+  methods: {
+    handleClick() {
+      localStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+  computed: {
+    ...mapGetters(["currentUser"]),
+    isLoggedIn() {
+      return this.currentUser.userId;
+    },
+  },
 };
 </script>
 
