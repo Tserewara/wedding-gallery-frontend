6ab804caa50c57765575d8dce84c4d164d9521b4
Tserewara <alvaro.alf2@gmail.com>
2022-05-04 15:39:07 -0300

    feat: ensure approve button only renders for admin users

--- src/presentation/components/card-photo/card-photo.vue
+++ src/presentation/components/card-photo/card-photo.vue
@@ -4,6 +4,7 @@
     <img :src="formatImageAddress(photo.image_address)" />
     <div class="likeWrap">
       <i class="fa-solid fa-heart"></i>
+      <button v-if="isAdmin">Approve Photo</button>
     </div>
     <CommentsContainer :comments="photo.comments" :photoId="photo._id" />
   </div>
@@ -11,23 +12,21 @@
 
 <script>
 import CommentsContainer from "@/presentation/components/comments-container/comments-container.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "CardPhoto",
   components: { CommentsContainer },
-
   props: {
     photo: Object,
-  },
-
-  data() {
-    return {};
   },
   methods: {
     formatImageAddress(address) {
       return `https://friends-gallery.s3.sa-east-1.amazonaws.com/${address}`;
     },
   },
+
+  computed: mapGetters(["isAdmin"]),
 };
 </script>
 
